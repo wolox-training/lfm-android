ae4d980dbe8e77c8b924b2342db03e453cf5537f
lfmontesanto <leandro.montesanto@wolox.com.ar>
2018-10-09 16:47:25 -0300

    mismo que el PR + @strings

--- logIn2/app/src/main/java/com/example/lmontesanto/login/MainActivity.java
+++ logIn2/app/src/main/java/com/example/lmontesanto/login/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.lmontesanto.login;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +16,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+   
 }
+
